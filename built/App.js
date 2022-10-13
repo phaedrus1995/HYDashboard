2d58bfd72ce20665afbedc18da0e3c5842d2eecc
Xi Zhang <72356406+phaedrus1995@users.noreply.github.com>
2022-10-13 14:47:17 +0800

    set up react&ts

---
+++ built/App.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var logo_svg_1 = require("./logo.svg");
+require("./App.css");
+function App() {
+    return (<div className="App">
+      <header className="App-header">
+        <img src={logo_svg_1.default} className="App-logo" alt="logo"/>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Learn React
+        </a>
+      </header>
+    </div>);
+}
+exports.default = App;
